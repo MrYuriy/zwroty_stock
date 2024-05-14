7888015da591123affac22ba40a0b3ef16c10837
MrYuriy <yuriybabiyk@gmail.com>
2024-05-14 20:20:36 +0200

    Add SKU Product Order and exstra models

--- zwroty/models.py
+++ zwroty/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    shop_nr = models.IntegerField()
+    description  = models.CharField(max_length=100)
+    ship_doc = models.CharField(max_length=4)
+
+    def __str__(self) -> str:
+        return f"{self.shop_nr} - {self.description}"
+
+
+class SkuInformation(models.Model):
+    sku = models.IntegerField()
+    name_of_product = models.CharField(max_length=100)
+    barcodes = models.ManyToManyField("Barcode")
+
+    def __str__(self):
+        return f"{self.sku} {self.name_of_product}"
+
+
+class Barcode(models.Model):
+    barcode = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.barcode
+
+
+class Product(models.Model):
+    sku = models.ForeignKey("SkuInformation", on_delete=models.CASCADE, null=True)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.sku.name_of_product
+
+
+class ReasoneComment(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ReturnOrder(models.Model):
+    nr_order = models.CharField(max_length=20)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    reasone = models.ForeignKey(ReasoneComment, on_delete=models.CASCADE)
+    products = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.nr_order} - {self.shop}"
+    
