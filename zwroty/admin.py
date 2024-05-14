54882783d93b562239d76f0000286992367c7487
MrYuriy <yuriybabiyk@gmail.com>
2024-05-14 20:20:09 +0200

    Register SKU Product Order and exstra models

--- zwroty/admin.py
+++ zwroty/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Shop, SkuInformation, Barcode, Product, ReasoneComment, ReturnOrder
+
+# Register your models here
+admin.site.register(Shop)
+admin.site.register(SkuInformation)
+admin.site.register(Barcode)
+admin.site.register(Product)
+admin.site.register(ReasoneComment)
+admin.site.register(ReturnOrder)
+
