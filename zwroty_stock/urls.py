4de3214d94dffa53dc6bbd4a9419b8f3e7e78498
MrYuriy <yuriybabiyk@gmail.com>
2024-05-15 17:50:22 +0200

    Add folder cache

--- zwroty_stock/urls.py
+++ zwroty_stock/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("zwroty.urls", namespace="zwroty")),
-    path("admin/", admin.site.urls),  
+    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("user/", include("user.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
